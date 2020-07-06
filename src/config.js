6a987ddbf2adbb9909eb5d432a85c400846f847c
NBESS <nicholasdbess@gmail.com>
2020-07-06 14:50:01 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "6b37770d3e274415b07071b4bc3c6253";
-export const redirectUri = "http://localhost:3000";
+export const redirectUri = "https://dreamy-brown-42de28.netlify.app";
 export const scopes = [
     "user-top-read",
     "playlist-modify-public",
