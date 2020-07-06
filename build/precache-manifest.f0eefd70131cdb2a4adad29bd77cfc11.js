f8f9ae616363d6955a98458c4610294a73d5d404
NBESS <nicholasdbess@gmail.com>
2020-07-06 14:02:01 -0400

    removes build from .gitignore

---
+++ build/precache-manifest.f0eefd70131cdb2a4adad29bd77cfc11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742dc19caff83fe178078c7eb31a277e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea0377c6ea1b757d74a5",
+    "url": "/static/css/main.870f1c2f.chunk.css"
+  },
+  {
+    "revision": "56936ef7f045db9f6065",
+    "url": "/static/js/2.ecf4d6a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ecf4d6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0377c6ea1b757d74a5",
+    "url": "/static/js/main.7b717e2f.chunk.js"
+  },
+  {
+    "revision": "950cfd921c20da47bd19",
+    "url": "/static/js/runtime-main.a317f607.js"
+  },
+  {
+    "revision": "9d3202142f862e93904bd88214eefc2e",
+    "url": "/static/media/unfocused-night-lights.9d320214.jpg"
+  }
+]);
